8cfebb1873d2fd79017132c1b894f5918e835be8
rinjugatla <rin.jugatla@gmail.com>
2024-01-24 09:37:31 +0900

    問い合わせ先を追加

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -296,6 +296,29 @@
 			</p>
 		</Alert>
 
+		<Alert class="mb-4" border color="blue">
+			<InfoCircleSolid slot="icon" class="h-4 w-4" />
+			<p>問い合わせ先</p>
+			<div class="flex">
+				<div class="mr-2">
+					<a
+					href="https://github.com/rinjugatla/palworld-setting-generator"
+					target="_blank"
+					rel="noopener noreferrer">GitHub</a
+				>
+				</div>
+				<div class="mx-2">
+					<a
+					href="https://twitter.com/rin_jugatla"
+					target="_blank"
+					rel="noopener noreferrer">Twitter/X: @rin_jugatla</a>
+				</div>
+				<div class="mx-2">
+					Discord: @rinjugatla
+				</div>
+			</div>
+		</Alert>
+
 		<Alert class="mb-4" border color="dark">
 			<div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
 				<div class="sm:col-span-2">
