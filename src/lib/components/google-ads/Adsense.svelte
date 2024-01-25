e7cde3c8e98e767ac272305ec0df88575022d000
rinjugatla <rin.jugatla@gmail.com>
2024-01-25 10:03:31 +0900

    Google Adsを追加
    ローカル環境では動作を確認できなかったのでリモート環境で動作するか確認

---
+++ src/lib/components/google-ads/Adsense.svelte
@@ -0,0 +1,7 @@
+<script lang="">
+	export let enabled = false;
+</script>
+
+<svelte:head>
+	<script data-ad-client="ca-pub-4388498410633493" async src={enabled ? 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js' : null}></script>
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
-<script>
+<script lang="ts">
 	import '../app.pcss';
+	import GoogleAdsense  from '$lib/components/google-ads/Adsense.svelte';
+
+	let promotionEnabled: boolean;
 </script>
+
+<GoogleAdsense  enabled={promotionEnabled} />
 
 <slot />
