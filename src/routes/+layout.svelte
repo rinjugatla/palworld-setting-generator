e7b208a1baeddd84eb936b3b750f51e4db3c0212
rinjugatla <rin.jugatla@gmail.com>
2024-01-25 10:55:09 +0900

    ページタイトルを設定

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,10 @@
 	import GoogleAdsense  from '$lib/components/google-ads/Adsense.svelte';
 </script>
 
+<svelte:head>
+	<title>Palworld server setting generator</title>
+</svelte:head>
+
 <GoogleAdsense/>
 
 <slot />
