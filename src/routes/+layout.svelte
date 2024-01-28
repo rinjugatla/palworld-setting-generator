305d35e471bf5140c06e747898ee194b72580e8b
rinjugatla <rin.jugatla@gmail.com>
2024-01-28 16:24:35 +0900

    google adsタグを追加

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,9 @@
 <script lang="ts">
 	import '../app.pcss';
-	import GoogleAdsense  from '$lib/components/google-ads/Adsense.svelte';
 </script>
 
 <svelte:head>
 	<title>Palworld server setting generator</title>
 </svelte:head>
-
-<GoogleAdsense/>
 
 <slot />
