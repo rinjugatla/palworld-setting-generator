e96bd54c802b638208289906e36c50ad57792323
rinjugatla <rin.jugatla@gmail.com>
2024-02-14 17:32:32 +0900

    Analyticsを削除

--- src/routes/privacy-policy/+page.svelte
+++ src/routes/privacy-policy/+page.svelte
@@ -11,15 +11,6 @@
 		<Faq>
 			<div>
 				<FaqItem>
-					<svelte:fragment slot="h3">アクセス解析について</svelte:fragment>
-					<p class="text-gray-500 dark:text-gray-400">
-						当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。このGoogleアナリティクスはデータの収集のためにCookieを使用しています。このデータは匿名で収集されており、個人を特定するものではありません。
-					</p>
-                    <p class="text-gray-500 dark:text-gray-400">
-						この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。この規約に関しての詳細は<a href="https://marketingplatform.google.com/about/analytics/terms/jp/" target="_blank" rel="noopener noreferrer">Googleアナリティクスサービス利用規約</a>のページや<a href="https://policies.google.com/technologies/ads?hl=ja" target="_blank" rel="noopener noreferrer">Googleポリシーと規約ページ</a>をご覧ください。
-					</p>
-				</FaqItem>
-				<FaqItem>
 					<svelte:fragment slot="h3">免責事項</svelte:fragment>
 					<p class="text-gray-500 dark:text-gray-400">
 						当サイトで提供される情報について一切の責任を負いません。
